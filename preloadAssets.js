b433364be638be444ec0e7c3ce9a2a963936ee28
HugoDavi777 <davibatista685@gmail.com>
2024-08-14 23:14:41 -0300

    Add files via upload

--- preloadAssets.js
+++ preloadAssets.js
@@ -3,21 +3,21 @@ function preloadAssets(scene){
     console.log(scene);
 
     // cenario
-    scene.load.image("fundo", "./assets/fundo.png")
+    scene.load.image("fundo", "fundo.png")
 
     // plataforma
-    scene.load.image("plataforma", "./assets/plataforma.png")
+    scene.load.image("plataforma", "plataforma.png")
 
     // player
-    scene.load.spritesheet("player", "./assets/player.png", {
+    scene.load.spritesheet("player", "player.png", {
         frameWidth: 32,
         frameHeight: 48
     });
 
     // coletavel
-    scene.load.image("star", "./assets/star.png");
+    scene.load.image("star", "star.png");
 
     // bombas
-    scene.load.image("bombs", "./assets/bomb.png");
+    scene.load.image("bombs", "bomb.png");
 
 }  
